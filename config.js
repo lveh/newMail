da545f6c299117b686e1d6ed5205fd5785798b28
lveh <leh570866058@163.com>
2024-01-14 12:13:24 +0800

    更新

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ module.exports = {
   location: '101190901',  // 和风天气-成都武侯区的id
   type: '1,3,9', // 和风天气-生活指数type
   tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
-  startDay: '2018-07-27', // 在一起的日期
+  startDay: '2023-09-27', // 在一起的日期
 }
