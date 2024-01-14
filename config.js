d70588396f7a507940c7e1393c67f30f2d268ade
lveh <leh570866058@163.com>
2024-01-15 01:15:17 +0800

    更新

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // 配置信息
 module.exports = {
-  fromDisplayText: '你辉哥哥', // 收件箱展示的来件人名字
+  fromDisplayText: '你老公', // 收件箱展示的来件人名字
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
   user: '570866058@qq.com', // 发送者邮箱
   pass: 'xemprbnfuzsqbbgf', // 发送者邮箱MTP协议密码
   to: ['570866058@qq.com'], // 发送到谁，填邮箱
-  weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
+  weatherKey: '225592789209405aaf4400a6c2e8cd39', // 和风天气key
   location: '101190901',  // 和风天气-成都武侯区的id
   type: '1,3,9', // 和风天气-生活指数type
-  tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
+  tianXingKey: '5487b72f419e8d6760f41c92b26105f9', // 天行数据的key
   startDay: '2023-09-27', // 在一起的日期
 }
